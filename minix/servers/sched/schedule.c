887496025fc891d37f4b48160aac6f2fdb629a3a
leyloo <Sondley1@gmail.com>
2017-04-11 04:07:06 -0400

    comit 12

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -389,3 +389,61 @@ static void balance_queues(minix_timer_t *tp)
 
 	set_timer(&sched_timer, balance_timeout, balance_queues, 0);
 }
+
+/*==========================================================================*
+  *				do_lottery				     *
+ *===========================================================================*/
+  int do_lottery()
+ {
+	struct schedproc *rmp;
+	int proc_nr;
+	int rv;
+	int lucky;
+	int old_priority;
+	int flag = -1;
+	int nTickets = 0;
+
+	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+		if ((rmp->flags & IN_USE) && PROCESS_IN_USER_Q(rmp)) {
+			if (USER_Q == rmp->priority) {
+				nTickets += rmp->ticketsNum;
+			}
+		}
+	}
+
+	lucky = nTickets ? rand() % nTickets : 0;
+	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+ 		if ((rmp->flags & IN_USE) && PROCESS_IN_USER_Q(rmp) &&
+ 				USER_Q == rmp->priority) {
+ 			old_priority = rmp->priority;
+ 			/* rmp->priority = USER_Q; */
+ 			if (lucky >= 0) {
+ 				lucky -= rmp->ticketsNum;
+ 				/*
+ 				   printf("lucky - %d = %d\n", rmp->ticketsNum, lucky);
+ 				 */
+ 				if (lucky < 0) {
+ 					rmp->priority = MAX_USER_Q;
+ 					flag = OK;
+ 					/* printf("endpoint %d\n", rmp->endpoint); */
+ 				}
+			}
+			if (old_priority != rmp->priority) {
++ 				schedule_process(rmp);
++ 			}
++ 		}
++ 	}
++ 	/*
++ 	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
++ 		if ((rmp->flags & IN_USE) && PROCESS_IN_USER_Q(rmp)) {
++ 			if (USER_Q == rmp->priority)
++ 				count_17++;
++ 			else if (MAX_USER_Q == rmp->priority)
++ 				count_16++;
++ 		}
++ 	}
++ 	printf("in 16: %d; in 17: %d\n", count_16, count_17); 
++ 	*/
++ 	/* printf("do_lottery OK? %d lucky=%d\n", flag, lucky); */
++ 	return nTickets ? flag : OK;
++ }
